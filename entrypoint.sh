24cffb7f4026dad18aa5b3e5ae6e47e0188143f7
Juan Gomez <jgomez@joinsaturn.com>
2021-03-03 19:45:27 -0500

    Fixed bumping pre-releases

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,203 +1,109 @@
-#!/bin/bash
+set -e
 
-set -o pipefail
+if [ -z "$GITHUB_TOKEN" ]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
 
-# config
-default_semvar_bump=${DEFAULT_BUMP:-minor}
+repo_fullname=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$repo_fullname.git
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Merge Action"
+
+
 with_v=${WITH_V:-false}
-release_branches=${RELEASE_BRANCHES:-master,main}
-custom_tag=${CUSTOM_TAG}
-source=${SOURCE:-.}
-dryrun=${DRY_RUN:-false}
-initial_version=${INITIAL_VERSION:-0.0.0}
-tag_context=${TAG_CONTEXT:-repo}
-tag_target_branch=${TAG_TARGET_BRANCH:-master}
-suffix=${PRERELEASE_SUFFIX:-beta}
-verbose=${VERBOSE:-true}
-
-cd ${GITHUB_WORKSPACE}/${source}
+branch=${BRANCH:-develop}
+default_semvar_bump=${BUMP:-patch}
+suffix=${PRERELEASE_SUFFIX:-alpha}
+stage_transition=${STAGE_TRANSITION:-false}
+should_bump=${SHOULD_BUMP:-false}
+pull_tag_from=${PULL_TAG_FROM:-HEAD}
 
 echo "*** CONFIGURATION ***"
+echo -e "\tCURRENT_BRANCH: ${branch}"
 echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
-echo -e "\tWITH_V: ${with_v}"
-echo -e "\tRELEASE_BRANCHES: ${release_branches}"
-echo -e "\tCUSTOM_TAG: ${custom_tag}"
-echo -e "\tSOURCE: ${source}"
-echo -e "\tDRY_RUN: ${dryrun}"
-echo -e "\tINITIAL_VERSION: ${initial_version}"
-echo -e "\tTAG_CONTEXT: ${tag_context}"
-echo -e "\tTAG_TARGET_BRANCH: ${tag_target_branch}"
-echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
-echo -e "\tVERBOSE: ${verbose}"
+echo -e "\tSUFFIX: ${suffix}"
+echo -e "\tSTAGE_TRANSITION: ${stage_transition}"
+echo -e "\tSHOULD_BUMP: ${should_bump}"
+echo -e "\tPULL_TAG_FROM: ${pull_tag_from}"
 
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-
+# Check if this is a pre-release branch
 pre_release="true"
-IFS=',' read -ra branch <<< "$release_branches"
-for b in "${branch[@]}"; do
-    echo "Is $b a match for ${current_branch}"
-    if [[ "${current_branch}" =~ $b ]]
-    then
-        pre_release="false"
-    fi
-done
-echo "pre_release = $pre_release"
-
-git fetch --tags
-
-
-# If release look for pre-tag on target branch
-if $pre_release
+if [[ "$branch" =~ "master" ]]
 then
-  tag=$(git tag --list --merged $tag_target_branch --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
-  pre_tag=$(git tag --list --merged $tag_target_branch --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-($suffix|alpha))$" | head -n1)
-  if [ -z "$pre_tag" ]
-  then
-    pre_tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-($suffix|alpha))$" | head -n1)
-  fi
-else
-  tag=$(git tag --list --merged $tag_target_branch --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
-  if [ -z "$tag"]
-  then
-    tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
-  fi
-  pre_tag=""
+    pre_release="false"
+fi
+echo "Pre-release: $pre_release"
+
+#set -o xtrace
+
+git fetch origin $branch
+git checkout $branch
+
+# Get latest tag
+tag=$(git tag --sort=-creatordate | head -n 1)
+# Get latest tag from branch
+branch_tag=$(git tag --merged $pull_tag_from --sort=-creatordate | head -n 1)
+echo "tag before latest check: $tag, $branch_tag"
+
+if [[ "$branch_tag" == *"$tag"* ]]; then
+  tag=$branch_tag
 fi
 
-if [ -z "$tag" ]
-then
-    log=$(git log --pretty='%B')
-    if [ -z "$pre_tag" ]
-    then
-      # If not tag or pre-tag found use INITIAL_VERSION
-      tag="$initial_version"
-      pre_tag="$initial_version"
-    else
-      if $pre_release
-      then
-        # If only pre-tag found use it if this is a pre-release
-        tag=$pre_tag
-      else
-        # If only pre-tag found remove pre-release suffix
-        tag=$(echo $pre_tag | sed -e "s/-.*//")
-      fi
-    fi
-
-else
-    log=$(git log $tag..HEAD --pretty='%B')
-fi
-
-echo -e "Existing tag ${tag}"
-echo -e "Existing pre-tag ${pre_tag}"
-
-
-# get current commit hash for tag
 tag_commit=$(git rev-list -n 1 $tag)
 
-# get current commit hash
+# get current commit hash for tag
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
     echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$tag
     exit 0
 fi
 
-# echo log if verbose is wanted
-if $verbose
-then
-  echo $log
+if [ "$tag" == "latest" ]; then
+    tag=$(git tag --sort=-creatordate | head -n 2 | tail -n 1)
 fi
 
-case "$log" in
-    *#major* ) new=$(semver -i major $tag); part="major";;
-    *#minor* ) new=$(semver -i minor $tag); part="minor";;
-    *#patch* ) new=$(semver -i patch $tag); part="patch";;
-    *#none* )
-        echo "Default bump was set to none. Skipping..."; exit 0;;
-    * )
-        if [ "$default_semvar_bump" == "none" ]; then
-            echo "Default bump was set to none. Skipping..."; exit 0
-        else
-            base_tag=$(echo $tag | sed -e "s/-.*//")
-            new=$(semver -i "${default_semvar_bump}" $base_tag); part=$default_semvar_bump
-        fi
-        ;;
-esac
+tag=$(echo $tag | sed -e "s/-.*//")
 
-echo $new $pre_tag
-if $pre_release
-then
-    # Already a prerelease available, bump it
-    new_pre_tag=$(echo $pre_tag | sed -e "s/-.*//")
-    new=$(semver -i "${default_semvar_bump}" $new_pre_tag --preid $suffix)-$suffix; part="$part"
+
+echo "tag before update: $tag"
+
+# if there are none or it's still latest or v, start tags at 0.0.0
+if [ -z "$tag" ] || [ "$tag" == "latest" ] || [ "$tag" == "v" ]; then
+    echo "Tag does not mmatch semver scheme X.Y.Z(-PRERELEASE)(+BUILD). Changing to 0.0.0'"
+    tag="0.0.0"
 fi
 
-echo $part
-
-# did we get a new tag?
-if [ ! -z "$new" ]
+if $should_bump
 then
-	# prefix with 'v'
-	if $with_v
-	then
-		new="v$new"
-	fi
-fi
-
-if [ ! -z $custom_tag ]
-then
-    new="$custom_tag"
+  new=$(semver -i $default_semvar_bump $tag);
+else
+  new=$tag
 fi
 
 if $pre_release
 then
-    echo -e "Bumping tag ${pre_tag}. \n\tNew tag ${new}"
-else
-    echo -e "Bumping tag ${tag}. \n\tNew tag ${new}"
+  new=$new-$suffix
+fi
+#
+if [ "$new" != "none" ]; then
+    # prefix with 'v'
+    if $with_v; then
+        new="v$new"
+    fi
+    echo "new tag: $new"
+
+    # push new tag ref to github
+    dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+    full_name=$GITHUB_REPOSITORY
+
+    echo "$dt: **pushing tag $new to repo $full_name"
+
+    git tag -a -m "release: ${new}" $new $commit
 fi
 
-# set outputs
-echo ::set-output name=new_tag::$new
-echo ::set-output name=part::$part
-
-#use dry run to determine the next tag
-if $dryrun
-then
-    echo ::set-output name=tag::$tag
-    exit 0
-fi
-
-echo ::set-output name=tag::$new
-
-## create local git tag
-git tag $new
-
-## push new tag ref to github
-dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-full_name=$GITHUB_REPOSITORY
-git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
-
-echo "$dt: **pushing tag $new to repo $full_name"
-
-git_refs_response=$(
-curl -s -X POST $git_refs_url \
--H "Authorization: token $GITHUB_TOKEN" \
--d @- << EOF
-
-{
-  "ref": "refs/tags/$new",
-  "sha": "$commit"
-}
-EOF
-)
-
-git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-
-echo "::debug::${git_refs_response}"
-if [ "${git_ref_posted}" = "refs/tags/${new}" ]; then
-  exit 0
-else
-  echo "::error::Tag was not created properly."
-  exit 1
-fi
+#git push origin :refs/tags/latest
+#git tag -fa -m "latest release" latest $commit
+git push --follow-tag
